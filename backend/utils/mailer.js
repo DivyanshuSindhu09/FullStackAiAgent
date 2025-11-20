2b3e1280f9242772578eb75d7f34bead1a4eb47f
DivyanshuSindhu09 <divyanshu.sindhu@gmail.com>
2025-11-20 22:59:09 +0530

    create nodemailer utility

---
+++ backend/utils/mailer.js
@@ -0,0 +1,25 @@
+import nodemailer from "nodemailer";
+
+export const sendMail = async (to, text, subject) => {
+    try {
+        const transporter = nodemailer.createTransport({
+        host: process.env.MAILTRAP_SMTP_HOST,
+        port: process.env.MAILTRAP_SMTP_PORT,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.MAILTRAP_SMTP_USER,
+            pass: process.env.MAILTRAP_SMTP_PASS,
+        },
+        });
+
+        const info = await transporter.sendMail({
+        from: '"Divyanshu Sindhu" <diyanshu.sindhu@gmail.com>',
+        to,
+        subject,
+        text, // plain‑text body
+        html: "<b>Hello world?</b>", // HTML body
+    });
+    } catch (error) {
+        
+    }
+}

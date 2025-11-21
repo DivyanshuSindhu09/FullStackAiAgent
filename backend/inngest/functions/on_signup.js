2da556d9661c4f4700bd9a3b63ee5f3fbb6f1036
DivyanshuSindhu09 <divyanshu.sindhu@gmail.com>
2025-11-21 22:37:24 +0530

    resuming the project

--- backend/inngest/functions/on_signup.js
+++ backend/inngest/functions/on_signup.js
@@ -5,6 +5,11 @@ export const onUserSignUp = inngest.createFunction(
     {event : "user/signup"}, //this function will be invoked whenever this event is triggered 
 
     async ({event, step}) => {
-
+        try {
+            //whoever is invoking some event they can pass some data
+            const {email} = event.data
+        } catch (error) {
+            
+        }
     }
 )
